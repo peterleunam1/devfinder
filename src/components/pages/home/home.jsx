94d952ec811952cde56a6712016af7f898557b57
peterleunan1 <arpm081001@gmail.com>
2023-02-20 17:21:05 -0500

    Fix: Translate texts

--- src/components/pages/home/home.jsx
+++ src/components/pages/home/home.jsx
@@ -1,16 +1,21 @@
 import Logotype from "../../molecules/Logotype/logotype";
 import Header from "../../organisms/header/header";
-import './home.scss';
+import "./home.scss";
 
 export default function Home() {
   return (
     <main className="home">
-     <span className="home__span"> <Logotype/></span>
-    <section className="banner">
-    <h1 className="home__title">¡Encuentra a Devs y ponte en contacto!</h1>
-     <p>Porque trabajar en equipo es la clave para construir una nueva sociedad y alcanzar el éxito.</p>
-      <Header/>
-    </section>
+      <span className="home__span">
+        <Logotype />
+      </span>
+      <section className="banner">
+        <h1 className="home__title">Find developers and get in contact!</h1>
+        <p>
+          Because teamwork is the key to building a new society and achieving
+          success.
+        </p>
+        <Header />
+      </section>
     </main>
   );
 }
